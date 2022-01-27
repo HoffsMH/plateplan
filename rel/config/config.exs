cefa3fa8bd0d45b54111a68ed139fc5345ce8768
HoffsMH <matthecker@pm.me>
2022-01-27 12:07:40 -0600

    look for PHX_HOST

--- rel/config/config.exs
+++ rel/config/config.exs
@@ -5,5 +5,5 @@ default_secret_key_base = :crypto.strong_rand_bytes(43) |> Base.encode64
 
 config :plateplan, PlateplanWeb.Endpoint,
   http: [port: port],
-  url: [host: "localhost", port: port],
+  url: [host: System.get_env("PHX_HOST") || "localhost", port: port],
   secret_key_base: System.get_env("SECRET_KEY_BASE") || default_secret_key_base
